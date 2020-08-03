7da1da6024ee38a25b186a5f4332c9ecd242dad3
nhuanthi <tlam1288@gmail.com>
2020-08-03 09:18:29 -0700

    pushing to heroku

--- models/Workout.js
+++ models/Workout.js
@@ -35,34 +35,10 @@ const WorkoutSchema = new Schema({
   ],
 });
 
+WorkoutSchema.virtual("total duration").get(function () {
+  return this.duration + duration;
+});
+
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
-
-// {
-//     day: new Date(new Date().setDate(new Date().getDate() - 10)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bicep Curl",
-//         duration: 20,
-//         weight: 100,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-
-// {
-//     day: new Date(new Date().setDate(new Date().getDate() - 9)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Lateral Pull",
-//         duration: 20,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
